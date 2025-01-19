1d8f8c17dc05ae028bdbd15960060feecebf02ae
Honkingbruh <101962901+Honkingbruh@users.noreply.github.com>
2025-01-18 16:49:58 -0800

    Update Electron.java

--- Electron.java
+++ Electron.java
@@ -1,4 +1,7 @@
 import java.awt.*;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Electron
 {
@@ -7,15 +10,42 @@ public class Electron
     double y;
     double vx;
     double vy;
-
-    public Electron(double initX, double initY)
+    ArrayList<Double[]> movements = new ArrayList<>();
+    ArrayList<Double> times = new ArrayList<>();
+    public Electron(double initX, double initY, double time)
     {
-        x = initX;
-        y = initY;
+        x = initX - 25;
+        y = initY - 25;
+        movements.add(new Double[]{x, y, time});
+        times.add(time);
     }
     public void update(Graphics g)
     {
         g.setColor(new Color(255,255,0));
-        g.fillOval((int)x, (int)y, 50, 50);
+        //System.out.println(x + " " + y);
+        g.fillOval((int)x - 25, (int)y - 25, 50, 50);
+    }
+    public void addCoords(double addX, double addY, double time)
+    {
+        x += addX;
+        y += addY;
+        movements.add(new Double[]{x,y,time});
+        times.add(time);
+        //System.out.println(x + " " + y + " " + time);
+    }
+    public Rectangle getBounds()
+    {
+        return new Rectangle((int)x - 25, (int)y, 50, 50);
+    }
+    public Double[] getPosAtTime(double time)
+    {
+        for(int i = 1; i < times.size(); i++)
+        {
+            if(times.get(i) > time)
+            {
+                return movements.get(i-1);
+            }
+        }
+        return movements.getLast();
     }
 }
