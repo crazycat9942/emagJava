8eb63bd20639a04cdb8d70a5732dba13fe1d004f
crazycat9942 <101962901+Honkingbruh@users.noreply.github.com>
2025-03-21 16:12:44 -0700

    Update Electron.java
    added 3d support

--- Electron.java
+++ Electron.java
@@ -1,3 +1,5 @@
+import javafx.geometry.Point3D;
+
 import java.awt.*;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
@@ -8,44 +10,51 @@ public class Electron
     int charge = -1;
     double x;
     double y;
+    double z;
     double fX;
     double fY;
+    double fZ;
     double mass = 9.1093897 * Math.pow(10, -31);
     ArrayList<Double[]> movements = new ArrayList<>();
     ArrayList<Double> times = new ArrayList<>();
-    public Electron(double initX, double initY, double time)
+    public Electron(double initX, double initY, double initZ, double time)
     {
         x = initX - 25;
         y = initY - 25;
-        movements.add(new Double[]{x, y, time});
+        z = initZ - 25;
+        movements.add(new Double[]{x, y, z, time});
         times.add(time);
     }
     public void update(Graphics g, Panel panel)
     {
-        Point2D.Double tempPoint = panel.getForce(x, y, false, charge);
-        fX += tempPoint.x * Math.cos(tempPoint.y);
-        fY += tempPoint.x * Math.sin(tempPoint.y);
+        Point3D tempPoint = panel.getForce(x, y, z, false, charge);
+        fX += tempPoint.getX() * Math.cos(tempPoint.getY());
+        fY += tempPoint.getX() * Math.sin(tempPoint.getY());
+        fZ += tempPoint.getZ() * Math.sin(tempPoint.getZ());
         if(panel.moveObjects && !(panel.parentSim.userPressed && getBounds().contains(panel.parentSim.lastPoint)))
         {
             x += fX * panel.timeStep * Math.pow(10, -21) / mass;
             y += fY * panel.timeStep * Math.pow(10, -21) / mass;
+            z += fZ * panel.timeStep * Math.pow(10, -21) / mass;
         }
         else if(panel.parentSim.userPressed && getBounds().contains(panel.parentSim.lastPoint))
         {
             fX = 0;
             fY = 0;
+            fZ = 0;
         }
-        movements.add(new Double[]{x,y,panel.time});
+        movements.add(new Double[]{x,y,z,panel.time});
         times.add(panel.time);
         g.setColor(new Color(255,255,0));
         //System.out.println(x + " " + y);
         g.fillOval((int)x - 25, (int)y - 25, 50, 50);
     }
-    public void addCoords(double addX, double addY, double time)
+    public void addCoords(double addX, double addY, double addZ, double time)
     {
         x += addX;
         y += addY;
-        movements.add(new Double[]{x,y,time});
+        z += addZ;
+        movements.add(new Double[]{x,y,z,time});
         times.add(time);
         //System.out.println(x + " " + y + " " + time);
     }
