8d11aa4582b50daf29ced497ad66eec7d087153f
crazycat9942 <101962901+Honkingbruh@users.noreply.github.com>
2025-03-21 10:49:28 -0700

    Update Electron.java

--- Electron.java
+++ Electron.java
@@ -8,8 +8,9 @@ public class Electron
     int charge = -1;
     double x;
     double y;
-    double vx;
-    double vy;
+    double fX;
+    double fY;
+    double mass = 9.1093897 * Math.pow(10, -31);
     ArrayList<Double[]> movements = new ArrayList<>();
     ArrayList<Double> times = new ArrayList<>();
     public Electron(double initX, double initY, double time)
@@ -19,8 +20,23 @@ public class Electron
         movements.add(new Double[]{x, y, time});
         times.add(time);
     }
-    public void update(Graphics g)
+    public void update(Graphics g, Panel panel)
     {
+        Point2D.Double tempPoint = panel.getForce(x, y, false, charge);
+        fX += tempPoint.x * Math.cos(tempPoint.y);
+        fY += tempPoint.x * Math.sin(tempPoint.y);
+        if(panel.moveObjects && !(panel.parentSim.userPressed && getBounds().contains(panel.parentSim.lastPoint)))
+        {
+            x += fX * panel.timeStep * Math.pow(10, -21) / mass;
+            y += fY * panel.timeStep * Math.pow(10, -21) / mass;
+        }
+        else if(panel.parentSim.userPressed && getBounds().contains(panel.parentSim.lastPoint))
+        {
+            fX = 0;
+            fY = 0;
+        }
+        movements.add(new Double[]{x,y,panel.time});
+        times.add(panel.time);
         g.setColor(new Color(255,255,0));
         //System.out.println(x + " " + y);
         g.fillOval((int)x - 25, (int)y - 25, 50, 50);
