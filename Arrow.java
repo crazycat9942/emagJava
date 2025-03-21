4c8191620211115645139c3782511e84c17359ac
crazycat9942 <101962901+Honkingbruh@users.noreply.github.com>
2025-03-21 16:12:24 -0700

    Update Arrow.java
    added 3d support

--- Arrow.java
+++ Arrow.java
@@ -1,9 +1,12 @@
+import javafx.geometry.Point3D;
+
 import java.awt.*;
 import java.awt.geom.Point2D;
 import java.math.*;
 public class Arrow
 {
-    double angle;
+    double angleTheta;
+    double anglePhi;
     double x;
     double y;
     double length = 10;
@@ -18,18 +21,19 @@ public class Arrow
     }
     public void update(Graphics g, Panel panel)
     {
-        Point2D tempPoint = panel.getForce(x, y, true, 1);
+        Point3D tempPoint = panel.getForce(x, y, 0, true, 1);
         magnitude = tempPoint.getX();
-        angle = tempPoint.getY();
+        angleTheta = tempPoint.getY();
+        anglePhi = tempPoint.getZ();
         panel.vector_to_rgb(this);
-        int endX = (int)(x + 2*length*Math.cos(angle));
-        int endY = (int)(y + 2*length*Math.sin(angle));
-        int leftX = endX - (int)(length*Math.sin(angle));
-        int leftY = endY + (int)(length*Math.cos(angle));
-        int rightX = endX + (int)(length*Math.sin(angle));
-        int rightY = endY - (int)(length*Math.cos(angle));
-        int topX = endX + (int)(length*Math.cos(angle));
-        int topY = endY + (int)(length*Math.sin(angle));
+        int endX = (int)(x + 2*length*Math.cos(angleTheta));
+        int endY = (int)(y + 2*length*Math.sin(angleTheta));
+        int leftX = endX - (int)(length*Math.sin(angleTheta));
+        int leftY = endY + (int)(length*Math.cos(angleTheta));
+        int rightX = endX + (int)(length*Math.sin(angleTheta));
+        int rightY = endY - (int)(length*Math.cos(angleTheta));
+        int topX = endX + (int)(length*Math.cos(angleTheta));
+        int topY = endY + (int)(length*Math.sin(angleTheta));
         g.setColor(color);
         Graphics2D g2d = (Graphics2D)g;
         g2d.setStroke(new BasicStroke(4));
@@ -52,18 +56,19 @@ public class Arrow
     }
     public void draw(Graphics g, Panel panel, Point userMouse)
     {
-        Point2D tempPoint = panel.getForce(userMouse.x, userMouse.y, true, 1);
+        Point3D tempPoint = panel.getForce(userMouse.x, userMouse.y, 0, true, 1);
         magnitude = tempPoint.getX();
-        angle = tempPoint.getY();
+        angleTheta = tempPoint.getY();
+        anglePhi = tempPoint.getZ();
         panel.vector_to_rgb(this);
-        int endX = (int)(x + 2*length*Math.cos(angle));
-        int endY = (int)(y + 2*length*Math.sin(angle));
-        int leftX = endX - (int)(length*Math.sin(angle));
-        int leftY = endY + (int)(length*Math.cos(angle));
-        int rightX = endX + (int)(length*Math.sin(angle));
-        int rightY = endY - (int)(length*Math.cos(angle));
-        int topX = endX + (int)(length*Math.cos(angle));
-        int topY = endY + (int)(length*Math.sin(angle));
+        int endX = (int)(x + 2*length*Math.cos(angleTheta));
+        int endY = (int)(y + 2*length*Math.sin(angleTheta));
+        int leftX = endX - (int)(length*Math.sin(angleTheta));
+        int leftY = endY + (int)(length*Math.cos(angleTheta));
+        int rightX = endX + (int)(length*Math.sin(angleTheta));
+        int rightY = endY - (int)(length*Math.cos(angleTheta));
+        int topX = endX + (int)(length*Math.cos(angleTheta));
+        int topY = endY + (int)(length*Math.sin(angleTheta));
         g.setColor(color);
         Graphics2D g2d = (Graphics2D)g;
         g2d.setStroke(new BasicStroke(4));
