9277a3d2c63625ac73c03aea37fd4cbad53a93f9
crazycat9942 <101962901+Honkingbruh@users.noreply.github.com>
2025-03-21 10:49:11 -0700

    Update Arrow.java

--- Arrow.java
+++ Arrow.java
@@ -17,8 +17,8 @@ public class Arrow
         y = initY;
     }
     public void update(Graphics g, Panel panel)
-    {//need to convert this to java code bc pasted it in from python
-        Point2D tempPoint = panel.getForce(x, y);
+    {
+        Point2D tempPoint = panel.getForce(x, y, true, 1);
         magnitude = tempPoint.getX();
         angle = tempPoint.getY();
         panel.vector_to_rgb(this);
@@ -49,5 +49,25 @@ public class Arrow
         pygame.draw.polygon(window,
 
                 vector_to_rgb(np.pi*rotation/180, length), (leftSide,rightSide,top),0)*/
+    }
+    public void draw(Graphics g, Panel panel, Point userMouse)
+    {
+        Point2D tempPoint = panel.getForce(userMouse.x, userMouse.y, true, 1);
+        magnitude = tempPoint.getX();
+        angle = tempPoint.getY();
+        panel.vector_to_rgb(this);
+        int endX = (int)(x + 2*length*Math.cos(angle));
+        int endY = (int)(y + 2*length*Math.sin(angle));
+        int leftX = endX - (int)(length*Math.sin(angle));
+        int leftY = endY + (int)(length*Math.cos(angle));
+        int rightX = endX + (int)(length*Math.sin(angle));
+        int rightY = endY - (int)(length*Math.cos(angle));
+        int topX = endX + (int)(length*Math.cos(angle));
+        int topY = endY + (int)(length*Math.sin(angle));
+        g.setColor(color);
+        Graphics2D g2d = (Graphics2D)g;
+        g2d.setStroke(new BasicStroke(4));
+        g2d.drawLine((int)x, (int)y, endX, endY);
+        g2d.drawPolygon(new int[] {leftX, rightX, topX},new int[] {leftY, rightY, topY}, 3);
     }
 }
