42e97b8d897b493df07de5b43ee662d70c39a8a2
crazycat9942 <101962901+crazycat9942@users.noreply.github.com>
2025-05-20 08:08:44 -0700

    Update FieldArrow.java

--- src/main/java/FieldArrow.java
+++ src/main/java/FieldArrow.java
@@ -5,26 +5,31 @@ import java.awt.BasicStroke;
 
 public class FieldArrow
 {
-    double angle;
+    double angleTheta;
+    double anglePhi;
     double x;
     double y;
-    double length = 30;
+    double z;
+    double length = 15;
     double endX;
     double endY;
+    double endZ;
     Color color;
     double magnitude;
     boolean drawMidpointArrow = false;
     FieldLine parent;
-    public FieldArrow(double initX, double initY, FieldLine parentLine)
+    public FieldArrow(double initX, double initY, double initZ, FieldLine parentLine)
     {
         x = initX;
         y = initY;
+        z = initZ;
         parent = parentLine;
     }
-    public void setCoords(double userX, double userY)
+    public void setCoords(double userX, double userY, double userZ)
     {
         x = userX;
         y = userY;
+        z = userZ;
     }
     public void update(Graphics g, Panel panel)
     {
@@ -41,7 +46,7 @@ public class FieldArrow
         boolean inElectron = false;
         for(Electron e: panel.electrons)
         {
-            if(e.getBounds().contains((int)endX, (int)endY))
+            if(e.getBounds(panel).contains((int)panel.CTSX(endX), (int)panel.CTSY(endY)))
             {
                 inElectron = true;
             }
@@ -50,18 +55,19 @@ public class FieldArrow
         {
             Graphics2D g2d = (Graphics2D)g;
             g2d.setStroke(new BasicStroke(4));
-            g2d.drawLine((int) x, (int) y, (int) endX, (int) endY);
+            g2d.drawLine((int) panel.CTSX(x), (int) panel.CTSY(y), (int) panel.CTSX(endX), (int) panel.CTSY(endY));
 
             if (drawMidpointArrow)
             {
-                double midpointX = (x + endX) / 2;
-                double midpointY = (y + endY) / 2;
-                double leftX = midpointX - (20 * Math.sin(angle));
-                double leftY = midpointY + (20 * Math.cos(angle));
-                double rightX = midpointX + (20 * Math.sin(angle));
-                double rightY = midpointY - (20 * Math.cos(angle));
-                double topX = midpointX + (20 * Math.cos(angle));
-                double topY = midpointY + (20 * Math.sin(angle));
+                angleTheta *= -1;
+                double midpointX = (panel.CTSX(x) + panel.CTSX(endX)) / 2;
+                double midpointY = (panel.CTSY(y) + panel.CTSY(endY)) / 2;
+                double leftX = midpointX - (20 * Math.sin(angleTheta)) * Math.sin(anglePhi);
+                double leftY = midpointY + (20 * Math.cos(angleTheta) * Math.sin(anglePhi));
+                double rightX = midpointX + (20 * Math.sin(angleTheta) * Math.sin(anglePhi));
+                double rightY = midpointY - (20 * Math.cos(angleTheta) * Math.sin(anglePhi));
+                double topX = midpointX + (20 * Math.cos(angleTheta) * Math.sin(anglePhi));
+                double topY = midpointY + (20 * Math.sin(angleTheta) * Math.sin(anglePhi));
                 g.drawLine((int) leftX, (int) leftY, (int) topX, (int) topY);
                 g.drawLine((int) rightX, (int) rightY, (int) topX, (int) topY);
             }
