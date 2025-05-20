4b8711e9a94b7d5856645f849755ae343f3c6ab9
crazycat9942 <101962901+crazycat9942@users.noreply.github.com>
2025-05-20 08:00:31 -0700

    Update FieldLine.java

--- FieldLine.java
+++ FieldLine.java
@@ -1,47 +1,58 @@
-import java.awt.*;
-import java.awt.geom.Point2D;
+import javafx.geometry.Point3D;
+
+import java.awt.Graphics;
 import java.util.ArrayList;
 
 public class FieldLine
 {
     double startX;
     double startY;
+    double startZ;
     ArrayList<FieldArrow> fieldArrows = new ArrayList<>();
     int fieldArrowNum = 200;
     int stopDrawing = -1;
-    public FieldLine(double initX, double initY)
+    double actualCharge = 1.602176634E-19;
+    public FieldLine(double initX, double initY, double initZ)
     {
         startX = initX;
         startY = initY;
+        startZ = initZ;
         for(int i = 0; i < fieldArrowNum; i++)
         {
-            fieldArrows.add(new FieldArrow(startX, startY, this));
+            fieldArrows.add(new FieldArrow(startX, startY, startZ, this));
         }
     }
     public void drawFieldArrows(Graphics g, Panel panel)
     {
         double fieldX = startX;
         double fieldY = startY;
+        double fieldZ = startZ;
         stopDrawing = -1;
         for(int i = 0; i < fieldArrowNum; i++)
         {
             if(stopDrawing == -1 || stopDrawing == i + 1)
             {
                 //System.out.println(fieldX + " " + fieldY);
-                Point2D tempPoint = panel.getForce(fieldX, fieldY, false, 1);
+                Point3D tempPoint = panel.getForce(fieldX, fieldY, fieldZ, false, actualCharge);
                 FieldArrow currArrow = fieldArrows.get(i);
                 currArrow.magnitude = tempPoint.getX();
                 //System.out.println(i + " " + tempArrow.angle);
-                currArrow.angle = tempPoint.getY();
-                currArrow.setCoords(fieldX, fieldY);
-                currArrow.endX = currArrow.x + currArrow.length * Math.cos(currArrow.angle) / 3;
-                currArrow.endY = currArrow.y + currArrow.length * Math.sin(currArrow.angle) / 3;
+                currArrow.angleTheta = tempPoint.getY();
+                currArrow.anglePhi = tempPoint.getZ();
+                currArrow.setCoords(fieldX, fieldY, fieldZ);
+                //currArrow.endX = currArrow.x + panel.mpp * currArrow.length * Math.cos(currArrow.angleTheta) * Math.sin(currArrow.anglePhi) / 3;
+                //currArrow.endY = currArrow.y + panel.mpp * currArrow.length * Math.sin(currArrow.angleTheta) * Math.sin(currArrow.anglePhi) / 3;
+                //currArrow.endZ = currArrow.z + panel.mpp * currArrow.length * Math.cos(currArrow.anglePhi) / 3;
+                currArrow.endX = currArrow.x + panel.mpp * currArrow.length * Math.cos(currArrow.angleTheta);
+                currArrow.endY = currArrow.y + panel.mpp * currArrow.length * Math.sin(currArrow.angleTheta);
+                currArrow.endZ = currArrow.z;
                 if (i % 24 == 12) {
                     currArrow.drawMidpointArrow = true;
                 }
                 currArrow.update(g, panel);
                 fieldX = currArrow.endX;
                 fieldY = currArrow.endY;
+                fieldZ = currArrow.endZ;
             }
         }
     }
