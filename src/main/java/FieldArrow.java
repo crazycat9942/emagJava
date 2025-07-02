8878ed1b1f06665f23a39582a61121bd4ce1d205
crazycat9942 <101962901+crazycat9942@users.noreply.github.com>
2025-07-01 18:12:38 -0700

    Update FieldArrow.java

--- src/main/java/FieldArrow.java
+++ src/main/java/FieldArrow.java
@@ -33,15 +33,6 @@ public class FieldArrow
     }
     public void update(Graphics g, Panel panel)
     {
-        //panel.vector_to_rgb(this);
-        /*endX = (x + 2*length*Math.cos(angle));
-        endY = (y + 2*length*Math.sin(angle));*/
-        /*int leftX = endX - (int)(length*Math.sin(angle));
-        int leftY = endY + (int)(length*Math.cos(angle));
-        int rightX = endX + (int)(length*Math.sin(angle));
-        int rightY = endY - (int)(length*Math.cos(angle));
-        int topX = endX + (int)(length*Math.cos(angle));
-        int topY = endY + (int)(length*Math.sin(angle));*/
         g.setColor(new Color(255, 255, 255));
         boolean inElectron = false;
         for(Electron e: panel.electrons)
@@ -55,7 +46,9 @@ public class FieldArrow
         {
             Graphics2D g2d = (Graphics2D)g;
             g2d.setStroke(new BasicStroke(4));
-            g2d.drawLine((int) panel.CTSX(x), (int) panel.CTSY(y), (int) panel.CTSX(endX), (int) panel.CTSY(endY));
+            if(!Double.isNaN(panel.CTSX(endX) + panel.CTSY(endY))) {
+                g2d.drawLine((int) panel.CTSX(x), (int) panel.CTSY(y), (int) panel.CTSX(endX), (int) panel.CTSY(endY));
+            }
 
             if (drawMidpointArrow)
             {
@@ -76,20 +69,5 @@ public class FieldArrow
         {
             parent.stopDrawing = parent.fieldArrows.indexOf(this);
         }
-        //g.drawPolygon(new int[] {leftX, rightX, topX},new int[] {leftY, rightY, topY}, 3);
-        /*
-        endX
-        length = Math.sqrt((end[0]-start[0])**2+(end[1]-start[1])**2)
-        rotation =np.atan2(end[0]-start[0],end[1]-start[1])
-        //end = (start[0] + length*np.cos(rotation), start[1] + length*np.sin(rotation))
-        color ='white'
-        //print(start)
-        pygame.draw.line(window,color,(start[0],start[1]),(end[0],end[1]),5)
-        leftSide =(end[0]-0.2*length*np.sin(rotation),end[1]+0.2*length*np.cos(rotation))
-        rightSide =(end[0]+0.2*length*np.sin(rotation),end[1]-0.2*length*np.cos(rotation))
-        top =(end[0]+0.2*length*np.cos(rotation),end[1]+0.2*length*np.sin(rotation))
-        pygame.draw.polygon(window,
-
-                vector_to_rgb(np.pi*rotation/180, length), (leftSide,rightSide,top),0)*/
     }
 }
