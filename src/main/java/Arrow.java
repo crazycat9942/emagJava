58cbd40af6dcd943f81eeed190b790cb9a246dd5
crazycat9942 <101962901+crazycat9942@users.noreply.github.com>
2025-07-01 18:05:59 -0700

    Update Arrow.java

--- src/main/java/Arrow.java
+++ src/main/java/Arrow.java
@@ -27,7 +27,7 @@ public class Arrow
     {
         Point3D posPoint = panel.rotate(panel.STCX(x),panel.STCY(y),0);
         posPoint = new Point3D(panel.STCX(x), panel.STCY(y),panel.centerOfMass().getZ());
-        Point3D tempPoint = panel.getForce(posPoint.getX(), posPoint.getY(), posPoint.getZ(), true, actualCharge);
+        Point3D tempPoint = panel.getForce(posPoint.getX(), posPoint.getY(), posPoint.getZ(), true, actualCharge, null);
         //Point3D tempPoint = panel.getForce(x,y,0,true,1);
         if(tempPoint.getX() != 0)
         {
@@ -46,28 +46,16 @@ public class Arrow
             g.setColor(color);
             Graphics2D g2d = (Graphics2D) g;
             g2d.setStroke(new BasicStroke(4));
-            g2d.drawLine((int) x, (int) y, endX, endY);
-            g2d.drawPolygon(new int[]{leftX, rightX, topX}, new int[]{leftY, rightY, topY}, 3);
+            if(endX + endY != 0) {
+                g2d.drawLine((int) x, (int) y, endX, endY);
+                g2d.drawPolygon(new int[]{leftX, rightX, topX}, new int[]{leftY, rightY, topY}, 3);
+            }
         }
-        /*
-        endX
-        length = Math.sqrt((end[0]-start[0])**2+(end[1]-start[1])**2)
-        rotation =np.atan2(end[0]-start[0],end[1]-start[1])
-        //end = (start[0] + length*np.cos(rotation), start[1] + length*np.sin(rotation))
-        color ='white'
-        //print(start)
-        pygame.draw.line(window,color,(start[0],start[1]),(end[0],end[1]),5)
-        leftSide =(end[0]-0.2*length*np.sin(rotation),end[1]+0.2*length*np.cos(rotation))
-        rightSide =(end[0]+0.2*length*np.sin(rotation),end[1]-0.2*length*np.cos(rotation))
-        top =(end[0]+0.2*length*np.cos(rotation),end[1]+0.2*length*np.sin(rotation))
-        pygame.draw.polygon(window,
-
-                vector_to_rgb(np.pi*rotation/180, length), (leftSide,rightSide,top),0)*/
     }
     public void draw(Graphics g, Panel panel, Point userMouse)
     {
         //Point3D tempPoint = panel.getForce2(panel.screenToCoordsX(userMouse.x), panel.screenToCoordsY(userMouse.y), 0, true, 1);
-        Point3D tempPoint = panel.getForce(panel.STCX(userMouse.x), panel.STCY(userMouse.y), panel.centerOfMass().getZ(), true, actualCharge);
+        Point3D tempPoint = panel.getForce(panel.STCX(userMouse.x), panel.STCY(userMouse.y), panel.centerOfMass().getZ(), true, actualCharge, null);
         if(tempPoint.getX() != 0)
         {
             //System.out.println(tempPoint);
